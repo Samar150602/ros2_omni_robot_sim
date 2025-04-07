6729ce3900aaa728a3bbd68912c7a159791e4c74
Yohan Prakoso <prakoso.yohan@gmail.com>
2025-04-07 21:56:52 +0800

    re structure the project to let change platform from env variable possible

--- launch/gazebo_sim.launch.py
+++ launch/gazebo_sim.launch.py
@@ -1,11 +1,12 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, EmitEvent
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command, TextSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
+from launch.events import Shutdown
 from pathlib import Path
 
 PACKAGE_NAME = "ros2_omni_robot_sim"
@@ -20,7 +21,26 @@ ARGUMENTS = [
             description='Use sim time if true'),
 ]
 
+robot_model_list = [
+    '3w', 
+    '3w_v2'
+]
+
 def generate_launch_description():
+    robot_model = os.environ.get("OMNI_ROBOT_MODEL", "3w")
+
+    if robot_model not in robot_model_list:
+        error_msg = f"The robot model specified in environment variable OMNI_ROBOT_MODEL is '{robot_model}', which is unknown.\nPlease choose from the following options:\n"
+
+        for model in robot_model_list:
+            error_msg += f"- {model}\n"
+        
+        print(error_msg)
+
+        return LaunchDescription([
+            EmitEvent(event=Shutdown(reason='Invalid robot model specified'))
+        ])
+
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
     # Source Environment (Need it to be able find mesh files)
@@ -35,7 +55,7 @@ def generate_launch_description():
 
     # Create a robot_state_publisher node
     pkg_path = get_package_share_directory(PACKAGE_NAME)
-    xacro_file = os.path.join(pkg_path,'urdf','3w_omni_robot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path,'urdf', robot_model, 'main.urdf.xacro')
     robot_description_config = Command(['xacro ', xacro_file])
     
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
@@ -63,12 +83,12 @@ def generate_launch_description():
     # Spawn the robot in Gazebo
     spawn_robot = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-name', '3w_omni_robot',
+                                   '-name', robot_model,
                                    '-z', '0.1'],
                         output='screen')
     
     # gz bridge 
-    bridge_params = os.path.join(get_package_share_directory(PACKAGE_NAME),'config','gz_bridge.yaml')
+    bridge_params = os.path.join(get_package_share_directory(PACKAGE_NAME),'config', 'gz_bridge', f'gz_bridge_{robot_model}.yaml')
     ros_gz_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
@@ -81,19 +101,34 @@ def generate_launch_description():
 
     
     # spawn controller 
-    spawn_wheel_controller = Node(package='controller_manager', executable='spawner',
-                        arguments=['joint_state_broadcaster', 
-                                    'wheel1_controller', 
-                                    'wheel2_controller', 
-                                    'wheel3_controller',
-                                    'camera_servo_controller'],
-                        output='screen')
+    if(robot_model == '3w'):
+        spawn_wheel_controller = Node(package='controller_manager', executable='spawner',
+                            arguments=['joint_state_broadcaster', 
+                                        'wheel1_controller', 
+                                        'wheel2_controller', 
+                                        'wheel3_controller',
+                                        'camera_servo_controller'],
+                            output='screen')
+        kinematics = Node(
+            package=PACKAGE_NAME,
+            executable="kinematics",
+            parameters=[{"use_sim_time": use_sim_time}]
+        )
+    else:
+        spawn_wheel_controller = Node(package='controller_manager', executable='spawner',
+                            arguments=['joint_state_broadcaster', 
+                                        'wheel1_controller', 
+                                        'wheel2_controller', 
+                                        'wheel3_controller',
+                                        ],
+                            output='screen')
+        kinematics = Node(
+            package=PACKAGE_NAME,
+            executable="kinematics_v2",
+            parameters=[{"use_sim_time": use_sim_time}]
+        )
 
-    kinematics = Node(
-        package=PACKAGE_NAME,
-        executable="kinematics",
-        parameters=[{"use_sim_time": use_sim_time}]
-    )
+
     
     # Create launch description and add actions
     ld = LaunchDescription(ARGUMENTS)
