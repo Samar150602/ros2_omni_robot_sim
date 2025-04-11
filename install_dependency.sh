b85e0567ca1516eaabcb2b0d529d726dfee16592
YePeOn7 <prakoso.yohan@gmail.com>
2025-04-11 07:08:17 +0000

    add install dependency

---
+++ install_dependency.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Gazebo Harmonic
+curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg && \
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null && \
+    sudo apt update && \
+    sudo apt install -y gz-harmonic
+
+sudo apt install -y ros-jazzy-ros-gz \
+  ros-jazzy-gz-ros2-control \
+  ros-jazzy-ros2-control \
+  ros-jazzy-ros2-controllers \
+  ros-jazzy-navigation2 \
+  ros-jazzy-nav2-bringup \
+  ros-jazzy-controller-manager
